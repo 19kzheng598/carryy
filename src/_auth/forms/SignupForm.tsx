5fde6b79960add425d15f0afae89228949eb196c
19kzheng598 <kevz3680@gmail.com>
2023-12-11 13:43:49 -0500

    ss

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -68,7 +68,7 @@ const SignupForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-300 flex-center flex-col">
-        <img src='public/icons/carry.svg' alt='carry logo' className="w-32 h-32"/>
+        <img src='dist/assets/icons/carry.svg' alt='carry logo' className="w-32 h-32"/>
 
         <h1 className="h2-bold">Welcome to Carry!</h1>
 
